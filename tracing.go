e018b9fee68d9fc023da0add42de453162067dcc
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2022-05-30 23:36:50 +0200

    Refactor extension and add push
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- tracing.go
+++ tracing.go
@@ -3,20 +3,18 @@ package xk6_client_tracing
 import (
 	"context"
 	"encoding/base64"
-	"errors"
+	"encoding/hex"
 	"fmt"
 	"math/rand"
 	"os"
 	"time"
 	"unsafe"
 
+	"github.com/dop251/goja"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/jaegerexporter"
 	log "github.com/sirupsen/logrus"
 	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/js/modules"
-	"go.k6.io/k6/lib"
-	"go.k6.io/k6/lib/netext"
-	"go.k6.io/k6/stats"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config"
@@ -31,7 +29,28 @@ import (
 )
 
 func init() {
-	modules.Register("k6/x/tracing", new(ClientTracing))
+	modules.Register("k6/x/tracing", new(tracingClientModule))
+}
+
+type ClientTracing struct {
+	vu modules.VU
+}
+
+type tracingClientModule struct{}
+
+var _ modules.Module = &tracingClientModule{}
+
+func (r *tracingClientModule) NewModuleInstance(vu modules.VU) modules.Instance {
+	return &ClientTracing{vu: vu}
+}
+
+func (r *ClientTracing) Exports() modules.Exports {
+	return modules.Exports{
+		Named: map[string]interface{}{
+			"Client":                r.xclient,
+			"generateRandomTraceID": r.generateRandomTraceID,
+		},
+	}
 }
 
 type exporter string
@@ -52,6 +71,12 @@ const (
 	thriftProtocol protocol = "thrift"
 )
 
+type Client struct {
+	exporter component.TracesExporter
+	cfg      *Config
+	vu       modules.VU
+}
+
 type Config struct {
 	Exporter       exporter `json:"type"`
 	Protocol       protocol `json:"protocol"`
@@ -63,12 +88,14 @@ type Config struct {
 	}
 }
 
-type ClientTracing struct {
-	exporter component.TracesExporter
-	cfg      *Config
-}
+func (c *ClientTracing) xclient(g goja.ConstructorCall) *goja.Object {
+	var cfg Config
+	rt := c.vu.Runtime()
+	err := rt.ExportTo(g.Argument(0), &cfg)
+	if err != nil {
+		common.Throw(rt, fmt.Errorf("Client constructor expects first argument to be Config"))
+	}
 
-func (c *ClientTracing) XClient(ctxPtr *context.Context, cfg Config) interface{} {
 	if cfg.Endpoint == "" {
 		cfg.Endpoint = "0.0.0.0:4317"
 	}
@@ -103,7 +130,7 @@ func (c *ClientTracing) XClient(ctxPtr *context.Context, cfg Config) interface{}
 			},
 		}
 	default:
-		return fmt.Errorf("failed to init exporter: unknown exporter type %s", cfg.Exporter)
+		log.Fatal(fmt.Errorf("failed to init exporter: unknown exporter type %s", cfg.Exporter))
 	}
 
 	exporter, err := factory.CreateTracesExporter(
@@ -119,121 +146,142 @@ func (c *ClientTracing) XClient(ctxPtr *context.Context, cfg Config) interface{}
 		exporterCfg,
 	)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 	exporter.Start(context.Background(), componenttest.NewNopHost())
 
 	if err != nil {
-		return fmt.Errorf("failed to create exporter: %v", err)
+		log.Fatal(fmt.Errorf("failed to create exporter: %v", err))
 	}
 
-	c.exporter = exporter
-	c.cfg = &cfg
-
-	rt := common.GetRuntime(*ctxPtr)
-	return common.Bind(rt, c, ctxPtr)
+	return rt.ToValue(&Client{
+		exporter: exporter,
+		cfg:      &cfg,
+		vu:       c.vu,
+	}).ToObject(rt)
 }
 
-func (c *ClientTracing) Send(ctx context.Context, spans []Span, debug bool) error {
+func (c *ClientTracing) generateRandomTraceID() string {
+	bytes := make([]byte, 16)
+	if _, err := rand.Read(bytes); err != nil {
+		return ""
+	}
+	return hex.EncodeToString(bytes)
+}
+
+type TraceEntry struct {
+	ID    string     `json:"id"`
+	Spans SpansEntry `json:"spans"`
+}
+
+type SpansEntry struct {
+	Count int `json:"count"`
+	Size  int `json:"size"`
+}
+
+func (c *Client) Push(te []TraceEntry) error {
 	traceData := pdata.NewTraces()
 
-	// Resource
 	rss := traceData.ResourceSpans()
-	rss.EnsureCapacity(1)
-	r := rss.AppendEmpty()
-	r.Resource().Attributes().InsertString("k6", "true")
-	r.Resource().Attributes().InsertString("service.name", "example")
+	rss.EnsureCapacity(len(te))
 
-	// Instrumentation Library
-	ilss := r.InstrumentationLibrarySpans()
-	ilss.EnsureCapacity(1)
-	ils := ilss.AppendEmpty()
-	ils.InstrumentationLibrary().SetName("k6")
-
-	// Span
-	sps := ils.Spans()
-	sps.EnsureCapacity(len(spans))
-	for e := 0; e < len(spans); e++ {
-		sps.AppendEmpty()
+	for _, t := range te {
+		traceData.ResourceSpans()
+		generateResource(t, rss.AppendEmpty())
 	}
 
-	for i, span := range spans {
-		cs := span.construct()
-		// This is a hack
-		if debug {
-			log.Info("Constructed span from TraceID: ", cs.TraceID().HexString())
-		}
-		cs.CopyTo(sps.At(i))
-	}
-
-	// Required for k6 metrics
-	state := lib.GetState(ctx)
-	if state == nil {
-		return errors.New("state required by k6 metrics is nil")
-	}
-	now := time.Now()
-
-	err := c.exporter.ConsumeTraces(ctx, traceData)
+	err := c.exporter.ConsumeTraces(context.Background(), traceData)
 	if err != nil {
 		return err
 	}
 
-	simpleNetTrail := netext.NetTrail{
-		BytesWritten: int64(unsafe.Sizeof(traceData)),
-		StartTime:    now.Add(-time.Minute),
-		EndTime:      now,
-		Samples: []stats.Sample{
-			{
-				Time:   now,
-				Metric: state.BuiltinMetrics.DataSent,
-				Value:  float64(unsafe.Sizeof(1)),
-			},
-		},
-	}
-	stats.PushIfNotDone(ctx, state.Samples, &simpleNetTrail)
-
-	stats.PushIfNotDone(ctx, state.Samples, stats.Sample{
-		Metric: stats.New("tracing_client_num_spans_sent", stats.Counter),
-		Time:   now,
-		Value:  float64(traceData.SpanCount()),
-	})
 	return nil
 }
 
-func (c *ClientTracing) SendDebug(ctx context.Context, spans []Span) error {
-	return c.Send(ctx, spans, true)
+func generateResource(t TraceEntry, dest pdata.ResourceSpans) {
+	dest.Resource().Attributes().InsertString("k6", "true")
+	dest.Resource().Attributes().InsertString("service.name", "k6")
+
+	ilss := dest.InstrumentationLibrarySpans()
+	ilss.EnsureCapacity(1)
+	ils := ilss.AppendEmpty()
+	ils.InstrumentationLibrary().SetName("k6")
+
+	// Spans
+	sps := ils.Spans()
+	sps.EnsureCapacity(t.Spans.Count)
+	for e := 0; e < t.Spans.Count; e++ {
+		generateSpan(t, sps.AppendEmpty())
+	}
 }
 
-func (c *ClientTracing) SendBytes(ctx context.Context, byteNumber int64, debug bool) error {
-	rand.Seed(time.Now().UTC().UnixNano())
+func generateSpan(t TraceEntry, dest pdata.Span) {
+	endTime := time.Now().Round(time.Second)
+	startTime := endTime.Add(-1 * time.Second)
 
-	var spans []Span
+	var b [16]byte
+	traceID, _ := hex.DecodeString(t.ID)
+	copy(b[:], traceID)
+
+	span := pdata.NewSpan()
+	span.SetTraceID(pdata.NewTraceID(b))
+	span.SetSpanID(newSegmentID())
+	span.SetParentSpanID(newSegmentID())
+	span.SetName(newString(15))
+	span.SetKind(pdata.SpanKindClient)
+	span.SetStartTimestamp(pdata.NewTimestampFromTime(startTime))
+	span.SetEndTimestamp(pdata.NewTimestampFromTime(endTime))
+	span.SetTraceState("x:y")
+
+	event := span.Events().AppendEmpty()
+	event.SetName(newString(12))
+	event.SetTimestamp(pdata.NewTimestampFromTime(startTime))
+	event.Attributes().InsertString(newString(12), newString(12))
+
+	status := span.Status()
+	status.SetCode(1)
+	status.SetMessage("OK")
+
+	attrs := pdata.NewAttributeMap()
+
+	// Fill the span with some random data
+	rand.Seed(time.Now().UTC().UnixNano())
 	var size int64
 	for {
-		if size >= byteNumber {
+		if size >= int64(t.Spans.Size) {
 			break
 		}
 
-		attr := make(map[string]interface{})
-		for i := 0; i <= rand.Intn(100-5)+5; i++ {
-			attr[newString(10)] = newString(10)
-		}
+		rKey := newString(rand.Intn(15))
+		rVal := newString(rand.Intn(15))
+		attrs.InsertString(rKey, rVal)
 
-		spans = append(spans, Span{
-			Name:       newString(30),
-			Attributes: attr,
-		})
-
-		size += int64(unsafe.Sizeof(spans))
+		size += int64(unsafe.Sizeof(rKey)) + int64(unsafe.Sizeof(rVal))
 	}
 
-	return c.Send(ctx, spans, debug)
+	attrs.CopyTo(span.Attributes())
+	span.CopyTo(dest)
 }
 
-func (c *ClientTracing) SendBytesDebug(ctx context.Context, byteNumber int64, debug bool) error {
-	return c.SendBytes(ctx, byteNumber, true)
+func (c *Client) Shutdown() error {
+	return c.exporter.Shutdown(context.Background())
 }
 
-func (c *ClientTracing) Shutdown(ctx context.Context) error {
-	return c.exporter.Shutdown(ctx)
+func newSegmentID() pdata.SpanID {
+	var r [8]byte
+	_, err := rand.Read(r[:])
+	if err != nil {
+		panic(err)
+	}
+	return pdata.NewSpanID(r)
+}
+
+func newString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
 }
